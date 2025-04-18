b1efe4b5e58664b533bd89abfa73e7f0fa341e70
Elizabeth <eli11jimenez11@gmail.com>
2025-04-17 19:46:15 -0500

    bellaqueo

--- backend/API_OPREF/urls.py
+++ backend/API_OPREF/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from .views import ContratoViewSet, CotizacionViewSet, ServicioViewSet, NovedadViewSet, PasswordResetRequestView, PasswordResetVerifyView
 from django.http import HttpResponse
+from .create_admin_view import CreateAdminView
 
 def login_welcome(request):
     return HttpResponse("Bienvenido a OPREF. Accede con tus credenciales.")
@@ -19,5 +20,5 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # La vista personalizada para la página de login
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Aquí puedes gestionar el login JWT
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset-request'),
-    path('password-reset/verify/', PasswordResetVerifyView.as_view(), name='password-reset-verify'),
+    path('password-reset/verify/', PasswordResetVerifyView.as_view(), name='password-reset-verify'),path('crear-admin/', CreateAdminView.as_view(), name='crear-admin'),
 ]
