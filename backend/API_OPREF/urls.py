c84ac92e80cbd84813850800d6b54182c9f50e1e
Elizabeth <eli11jimenez11@gmail.com>
2025-04-18 18:43:03 -0500

    si no fue no fue

--- backend/API_OPREF/urls.py
+++ backend/API_OPREF/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
-from .views import ContratoViewSet, CotizacionViewSet, ServicioViewSet, NovedadViewSet, PasswordResetRequestView, PasswordResetVerifyView
+from .views import (
+    ContratoViewSet, CotizacionViewSet, ServicioViewSet, NovedadViewSet,
+    PasswordResetRequestView, PasswordResetVerifyView, CustomTokenObtainPairView
+)
 from django.http import HttpResponse
 from .create_admin_view import CreateAdminView
 
@@ -16,7 +19,7 @@ router.register(r'novedades', NovedadViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),  # Rutas de la API
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # La vista personalizada para la página de login
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Aquí puedes gestionar el login JWT
     path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset-request'),
