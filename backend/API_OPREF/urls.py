14553fc0dff37333ada56f0f93bc87a0299b4679
Elizabeth <eli11jimenez11@gmail.com>
2025-04-18 19:19:01 -0500

    tatataca

--- backend/API_OPREF/urls.py
+++ backend/API_OPREF/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
-from django.contrib import admin
 from .views import (
     ContratoViewSet, CotizacionViewSet, ServicioViewSet, NovedadViewSet,
     PasswordResetRequestView, PasswordResetVerifyView, CustomTokenObtainPairView
 )
 from django.http import HttpResponse
 from .create_admin_view import CreateAdminView
+from django.contrib import admin
 
 def login_welcome(request):
     return HttpResponse("Bienvenido a OPREF. Accede con tus credenciales.")
@@ -20,10 +20,10 @@ router.register(r'novedades', NovedadViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # <- cambio aquí
-    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/password-reset/', PasswordResetRequestView.as_view(), name='password-reset-request'),
-    path('api/password-reset/verify/', PasswordResetVerifyView.as_view(), name='password-reset-verify'),
-    path('api/crear-admin/', CreateAdminView.as_view(), name='crear-admin'),
+    path('', include(router.urls)),  # Rutas de la API
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # La vista personalizada para la página de login
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset-request'),
+    path('password-reset/verify/', PasswordResetVerifyView.as_view(), name='password-reset-verify'),
+    path('crear-admin/', CreateAdminView.as_view(), name='crear-admin'),
 ]
