29ce2c33491b2cf43ccb3803bc5c61f075fd3c86
Elizabeth <eli11jimenez11@gmail.com>
2025-04-18 19:12:34 -0500

    PRILIN

--- backend/API_OPREF/urls.py
+++ backend/API_OPREF/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
+from django.contrib import admin
 from .views import (
     ContratoViewSet, CotizacionViewSet, ServicioViewSet, NovedadViewSet,
     PasswordResetRequestView, PasswordResetVerifyView, CustomTokenObtainPairView
@@ -18,6 +19,7 @@ router.register(r'servicios', ServicioViewSet)
 router.register(r'novedades', NovedadViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # <- cambio aquí
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
