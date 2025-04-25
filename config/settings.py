1d8243b1b0869e5b77a59dc1f12d33e5385b1724
Elizabeth <eli11jimenez11@gmail.com>
2025-04-25 15:25:50 -0500

    melam

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Configuración de CORS (Si tu frontend está en un dominio diferente)
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000/",  # Cambia por la URL de tu frontend si usas otro puerto
+    "https://appi-ytm1.onrender.com/",  # Si estás trabajando con un dominio en producción
+]
 
 
 # Configuración de seguridad en Render
